692580756a64daf7a675d01dd39c79e0c60b219b
Lucas Vandroux <lucas.vandroux@gmail.com>
2019-05-04 16:02:08 +0200

    Change the syntax to work with the tar file to make sure it is closed once the process is terminated.

--- rockethub/rocket.py
+++ rockethub/rocket.py
@@ -21,10 +21,9 @@ def unpack_tar_to_rocket(tar_path: str, rocket_folder_name: str, folder_path: st
     Unpack a TAR archive in a specific folder, rename it and then remove the tar file (or not if the user wants to)
 
     """
-    t = tarfile.open(tar_path, 'r')
-    tar_folder_name = os.path.commonprefix(t.getnames())
-    t.extractall(folder_path) # unpack in the wrong folder
-    t.close()
+    with tarfile.open(tar_path, 'r') as t:
+        tar_folder_name = os.path.commonprefix(t.getnames())
+        t.extractall(folder_path) # unpack in the wrong folder
 
     # Should rename the folder once it is unpacked
     rocket_folder_path = os.path.join(folder_path, rocket_folder_name)
